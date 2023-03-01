cb0dd3e6c72d104310e7c4059a18e75f4cb4ff49
Rafael Rodríguez Martínez <48961409+RafaelRodMar@users.noreply.github.com>
2023-03-01 18:41:52 +0100

    Update EditorState.cpp

--- sdlimguieditor/EditorState.cpp
+++ sdlimguieditor/EditorState.cpp
@@ -25,10 +25,12 @@ void EditorState::render()
 {
 	if (Editor::Instance()->project == nullptr) return;
 
+	auto& vecScenes = Editor::Instance()->project->ventities;
+
 	//imgui
 
-	static float f = 0.0f;
-	static int counter = 0;
+	//static float f = 0.0f;
+	//static int counter = 0;
 	Entity entitySelected;
 
 	//one window
@@ -42,9 +44,9 @@ void EditorState::render()
 		{
 			if (ImGui::MenuItem("Add scene"))
 			{
-				if (Editor::Instance()->project->ventities.size() < 256)
+				if (vecScenes.size() < 256)
 				{
-					Editor::Instance()->project->ventities.push_back(Entity("scene", "scene"));
+					vecScenes.push_back(Entity("scene", "scene"));
 				}
 			}
 			ImGui::EndPopup();
@@ -54,20 +56,20 @@ void EditorState::render()
 		//  You may retain selection state inside or outside your objects in whatever format you see fit.
 		// 'node_clicked' is temporary storage of what node we have clicked to process selection at the end
 		/// of the loop. May be a pointer to your own node type, etc.
-		static std::vector<bool> selection_mask(256, false);
-		int node_clicked = -1;
-		for (int i = 0; i < Editor::Instance()->project->ventities.size(); i++) {
+		static std::vector<bool> selection_mask_scenes(256, false);
+		int node_clicked_scenes = -1;
+		for (int i = 0; i < vecScenes.size(); i++) {
 			// Disable the default "open on single-click behavior" + set Selected flag according to our selection.
 			// To alter selection we use IsItemClicked() && !IsItemToggledOpen(), so clicking on an arrow doesn't alter selection.
-			ImGuiTreeNodeFlags node_flags = ImGuiTreeNodeFlags_OpenOnArrow | ImGuiTreeNodeFlags_OpenOnDoubleClick | ImGuiTreeNodeFlags_SpanAvailWidth;
-			const bool is_selected = selection_mask[i] != false;
+			ImGuiTreeNodeFlags node_flags_scenes = ImGuiTreeNodeFlags_OpenOnArrow | ImGuiTreeNodeFlags_OpenOnDoubleClick | ImGuiTreeNodeFlags_SpanAvailWidth;
+			const bool is_selected = selection_mask_scenes[i] != false;
 			if (is_selected)
-				node_flags |= ImGuiTreeNodeFlags_Selected;
+				node_flags_scenes |= ImGuiTreeNodeFlags_Selected;
 
-			bool node_open = ImGui::TreeNodeEx((void*)(intptr_t)i, node_flags, Editor::Instance()->project->ventities[i].name.c_str());
+			bool node_open = ImGui::TreeNodeEx((void*)(intptr_t)i, node_flags_scenes, vecScenes[i].name.c_str());
 			if (ImGui::IsItemClicked() && !ImGui::IsItemToggledOpen())
 			{
-				node_clicked = i;
+				node_clicked_scenes = i;
 				Editor::Instance()->project->selected = i;
 			}
 
@@ -78,12 +80,12 @@ void EditorState::render()
 				if (ImGui::MenuItem("Add Layer"))
 				{
 					Editor::Instance()->project->selected = i;
-					Editor::Instance()->project->ventities[Editor::Instance()->project->selected].ventities.push_back(Entity("layer", "layer"));
+					vecScenes[Editor::Instance()->project->selected].ventities.push_back(Entity("layer", "layer"));
 				}
 				ImGui::Separator();
 				if (ImGui::MenuItem("Remove scene"))
 				{
-					Editor::Instance()->project->ventities.erase(Editor::Instance()->project->ventities.begin() + i);
+					vecScenes.erase(Editor::Instance()->project->ventities.begin() + i);
 					Editor::Instance()->project->selected = -1;
 				}
 				ImGui::EndPopup();
@@ -93,9 +95,9 @@ void EditorState::render()
 			{
 				//show layers
 				//node_flags = ImGuiTreeNodeFlags_OpenOnArrow | ImGuiTreeNodeFlags_OpenOnDoubleClick | ImGuiTreeNodeFlags_SpanAvailWidth;
-				node_flags = ImGuiTreeNodeFlags_None | ImGuiTreeNodeFlags_Leaf | ImGuiTreeNodeFlags_NoTreePushOnOpen; //leaf node
-				for (int j = 0; j < Editor::Instance()->project->ventities[i].ventities.size(); j++) {
-					ImGui::TreeNodeEx((void*)(intptr_t)j, node_flags, Editor::Instance()->project->ventities[i].ventities[j].name.c_str());
+				ImGuiTreeNodeFlags node_flags = ImGuiTreeNodeFlags_None | ImGuiTreeNodeFlags_Leaf | ImGuiTreeNodeFlags_NoTreePushOnOpen; //leaf node
+				for (int j = 0; j < vecScenes[i].ventities.size(); j++) {
+					ImGui::TreeNodeEx((void*)(intptr_t)j, node_flags, vecScenes[i].ventities[j].name.c_str());
 
 					//context menu
 					if (ImGui::BeginPopupContextItem()) // <-- use last item id as popup id
@@ -114,16 +116,16 @@ void EditorState::render()
 			}
 		}
 
-		if (node_clicked != -1)
+		if (node_clicked_scenes != -1)
 		{
 			// Update selection state
 			// (process outside of tree loop to avoid visual inconsistencies during the clicking frame)
 			if (ImGui::GetIO().KeyCtrl)
-				selection_mask[node_clicked] = !selection_mask[node_clicked];          // CTRL+click to toggle
+				selection_mask_scenes[node_clicked_scenes] = !selection_mask_scenes[node_clicked_scenes];          // CTRL+click to toggle
 			else //if (!(selection_mask & (1 << node_clicked))) // Depending on selection behavior you want, may want to preserve selection when clicking on item that is part of the selection
 			{
-				std::fill(selection_mask.begin(), selection_mask.end(), 0);
-				selection_mask[node_clicked] = true;           // Click to single-select
+				std::fill(selection_mask_scenes.begin(), selection_mask_scenes.end(), 0);
+				selection_mask_scenes[node_clicked_scenes] = true;           // Click to single-select
 			}
 		}
 
@@ -137,10 +139,10 @@ void EditorState::render()
 	if (Editor::Instance()->project->selected != -1)
 	{
 		ImGui::Text("Project element : %d", Editor::Instance()->project->selected);
-		ImGui::Text("Name : %s", Editor::Instance()->project->ventities[Editor::Instance()->project->selected].name);
-		ImGui::Text("Type : %s", Editor::Instance()->project->ventities[Editor::Instance()->project->selected].type);
-		ImGui::Text("Position : %d, %d", Editor::Instance()->project->ventities[Editor::Instance()->project->selected].pos.x, Editor::Instance()->project->ventities[Editor::Instance()->project->selected].pos.y);
-		ImGui::InputText("archive", &Editor::Instance()->project->ventities[Editor::Instance()->project->selected].path);
+		ImGui::Text("Name : %s", vecScenes[Editor::Instance()->project->selected].name);
+		ImGui::Text("Type : %s", vecScenes[Editor::Instance()->project->selected].type);
+		ImGui::Text("Position : %d, %d", vecScenes[Editor::Instance()->project->selected].pos.x, vecScenes[Editor::Instance()->project->selected].pos.y);
+		ImGui::InputText("archive", &vecScenes[Editor::Instance()->project->selected].path);
 	}
 	ImGui::End();
 
